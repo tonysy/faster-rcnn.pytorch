d39604e5c9b453d6d14a2293ce085d7b56984d32
syzhang <syzhang@nullmax.ai>
2018-07-11 10:45:02 +0800

    add utils folder and meta branch

---
+++ utils/plot_tsne.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------
+# Few-shot for Object Detection
+# Copyright (c) 2016 by Contributors
+# Copyright (c) 2018 Plus Group, ShanghaiTech University
+# Licensed under The Apache-2.0 License [see LICENSE for details]
+# Written by Songyang Zhang
+# --------------------------------------------------------
+
+
+
+# generate model and load weight dict
+# get activations of trainning data(all ROIs)
+
